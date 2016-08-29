60994da73a299ab2602f0a5001a35c99c19d44d7
tzolotuhin <tzolotuhin@gmail.com>
2016-08-29 23:48:29 +0700

    Endpoint first commit

---
+++ endpoint/src/main/java/ru/nsu/fit/endpoint/rest/HelloWorldService.java
@@ -0,0 +1,20 @@
+package ru.nsu.fit.endpoint.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Timur Zolotuhin (tzolotuhin@gmail.com)
+ */
+@Path("/hello")
+public class HelloWorldService {
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+    }
+}
