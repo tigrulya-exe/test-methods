591b31bdba2d04c60fdb01f7bfac6afe5698b304
Timur Zolotuhin <tzolotuhin@odin.com>
2019-09-05 18:12:48 +0700

    Refactoring...

--- endpoint_tests/src/main/java/ru/nsu/fit/services/browser/Browser.java
+++ endpoint_tests/src/main/java/ru/nsu/fit/services/browser/Browser.java
@@ -2,6 +2,8 @@ package ru.nsu.fit.services.browser;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,6 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.nsu.fit.shared.ImageUtils;
 
 import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
@@ -25,8 +28,37 @@ public class Browser implements Closeable {
     public Browser() {
         // create web driver
         try {
-            System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
-            webDriver = new ChromeDriver();
+            ChromeOptions chromeOptions = new ChromeOptions();
+
+            // for running in Docker container as 'root'.
+            chromeOptions.addArguments("no-sandbox");
+            chromeOptions.addArguments("disable-dev-shm-usage");
+            chromeOptions.addArguments("disable-setuid-sandbox");
+            chromeOptions.addArguments("disable-infobars");
+
+            chromeOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+            chromeOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+
+            // we use Windows platform for development only and not for AT launch.
+            // For launch AT regression, we use Linux platform.
+            if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
+                chromeOptions.setHeadless(Boolean.parseBoolean(System.getProperty("headless")));
+                webDriver = new ChromeDriver(chromeOptions);
+            } else {
+                File f = new File("/usr/bin/chromedriver");
+                if (f.exists()) {
+                    chromeOptions.addArguments("single-process");
+                    chromeOptions.addArguments("headless");
+                    System.setProperty("webdriver.chrome.driver", f.getPath());
+                    webDriver = new ChromeDriver(chromeOptions);
+                }
+            }
+
+            if (webDriver == null) {
+                throw new RuntimeException();
+            }
+
             webDriver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
